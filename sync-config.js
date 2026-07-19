655642bc40d4bae7c8c5bf88b96487d4f4580c66
DESKTOP-KB7CMGL\admin <solbeenShin@lgcns.com>
2026-07-19 17:48:15 +0900

    v0.51

---
+++ sync-config.js
@@ -0,0 +1,7 @@
+﻿window.ACNH_SYNC_CONFIG = {
+  provider: "supabase",
+  projectUrl: "https://ytkajyvvqmjasnfjemqp.supabase.co",
+  anonKey: "sb_publishable_proJ-M2Vr_yl985pViEmfg_4A_tKynA",
+  table: "acnh_shared_state",
+  rowId: "main",
+};
